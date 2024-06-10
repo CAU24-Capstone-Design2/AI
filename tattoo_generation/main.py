6ab09838918e2a406e5ce0ac0a1f28a70459ebed
Melan <junhee329@naver.com>
2024-06-10 23:31:00 +0900

    feat: Complete project development

--- tattoo_generation/main.py
+++ tattoo_generation/main.py
@@ -1,5 +1,5 @@
 # diffuser
-from diffusers import StableDiffusionXLPipeline, StableDiffusionXLImg2ImgPipeline
+from diffusers import StableDiffusionXLPipeline, StableDiffusionXLImg2ImgPipeline, AutoPipelineForInpainting, AutoencoderKL
 from diffusers.utils import load_image, make_image_grid
 import torch
 # default lib
@@ -15,112 +15,138 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-u', '--userid', type=str, help='enter user id')
     parser.add_argument('-f', '--filename', type=str, help='enter file name')
-    parser.add_argument('-d', '--debug', action='store_true', help='debugging mode')
     parser.add_argument('-p', '--prompt', type=str, help='image generation prompt')
-    parser.add_argument('-l', '--lora', action='store_true', help='Apply tattoo style LoRA')
+    parser.add_argument('-l', '--lora', type=str, required=False, help='Apply tattoo style LoRA')
+    parser.add_argument('-i', '--inpaint', action='store_true', help='use inpainting model instead refiner')
+    parser.add_argument('-d', '--debug', action='store_true', help='debugging mode')
     args = parser.parse_args()    
 
-
-    # [default] project directory path (hardcoded)
+    # project directory path (hardcoded)
     home_dir = os.path.expanduser('~')
-    proj_dir = 'junhee/scart'
+    proj_dir = 'path/to/proj/dir'
     # arguments
-    userid = args.userid if args.userid else 'user1'
-    filename = args.filename if args.filename else 'burn.png'
-    prompt = args.prompt if args.prompt else "Astronaut floating in the space"
-    img_gen_prompt = prompt + ", tattoo design, white background, center align, big object" 
-    lora = True if args.lora else False
-    # [determined] base model & lora path
-    models_dir = f'{home_dir}/{proj_dir}/tattoo_generation/models'
-    model_path = f'{models_dir}/sdxl_base_1.0.safetensors'
-    lora_path = f'{models_dir}/lora/Traditional_Tattoos.safetensors'
-    # [determined] mask & tattoo & inpaint path
+    userid = args.userid 
+    filename = args.filename
+    # prompt = args.prompt + ", with white background, center align, big object" 
+    # prompt = args.prompt + ", tattoo design, white background, no gradations, no shadows, clear contrast between background and foreground, intricate line work, center align, big object"
+    prompt = args.prompt
+    lora = args.lora.replace(' ', '').lower() if args.lora else None
+    use_inpaint = args.inpaint
+    # path
     input_path = f'{home_dir}/{proj_dir}/images/{userid}/inputs/{filename}'
     mask_path = f'{home_dir}/{proj_dir}/images/{userid}/masks/{filename}'
     tattoo_path = f'{home_dir}/{proj_dir}/images/{userid}/tattoos/{filename}'
-    inpaint_path = f'{home_dir}/{proj_dir}/images/{userid}/inpaint/{filename}'
-    # sampling hyper parameter
-    num_inference_steps = 30
-    lora_scale = 0.8
+    synthesis_path = f'{home_dir}/{proj_dir}/images/{userid}/synthesis/{filename}'
     # integrated image path
     results_path = 'results/result.png'
     # device
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-    # initialize a wandb run
-    wandb.init(project='ScArt')
+    # Initialize wandb run
+    wandb.init(project='ScArt', allow_val_change=True)
     wandb.run.name = 'tattoo-generation'
     wandb.run.save() 
 
     config = wandb.config
     config.base_checkpoint = "stabilityai/stable-diffusion-xl-base-1.0"
     config.refiner_checkpoint = "stabilityai/stable-diffusion-xl-refiner-1.0"
-    config.prompt = img_gen_prompt
+    config.inpaint_checkpoint = "diffusers/stable-diffusion-xl-1.0-inpainting-0.1"
+    config.lora_checkpoint = f'melancholic/{lora}_tattoo_lora' if lora else None
+    config.prompt = f"In the style of TOK, {prompt} on the white background, main object or character on the center" if lora else prompt
     config.num_inference_steps = 30
-    config.lora_scale = 0.65
+    config.lora_scale = 0.8
+    config.inpaint_num_inference_steps = 20     # steps between 15 and 30 work well for us
+    config.inpaint_guidance_scale = 12.0  
+    config.inpaint_strength = 0.7              # make sure to use `strength` below 1.0
     wandb.config.update(config)
 
+
+    # Generate tattoo 
+    vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
     base_pipe = StableDiffusionXLPipeline.from_pretrained(
         config.base_checkpoint,
+        vae=vae,
         torch_dtype=torch.float16,
         variant='fp16',
-        use_safetensors=True
+        use_safetensors=True,
     ).to(device)
 
-    # Sampling tattoo 
-    if lora:
-        base_pipe.load_lora_weights(lora_path, adapter_name="tattoo")
-        draft = base_pipe(prompt=config.prompt, num_inference_steps=config.num_inference_steps, 
-                          cross_attention_kwargs={"scale": config.lora_scale}).images[0]
+    if config.lora_checkpoint:
+        base_pipe.load_lora_weights(config.lora_checkpoint, adapter_name="tattoo")
+        generator = torch.Generator(device="cuda").manual_seed(0)
+        draft = base_pipe(prompt=config.prompt, num_inference_steps=config.num_inference_steps, generator=generator).images[0]
     else:
         draft = base_pipe(prompt=config.prompt, num_inference_steps=config.num_inference_steps).images[0]
 
-    # save tattoo image
-    draft.save(tattoo_path)
 
-    # refiner pipeline : sdxl refiner 1.0
-    refiner_pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained(
-        config.refiner_checkpoint, 
-        torch_dtype=torch.float16, 
-        variant="fp16", 
-        use_safetensors=True
-    ).to(device)
+    # Search mask position
+    wnd_mask = load_image(mask_path).resize((1024, 1024))
+    wnd_bbox, crop_wnd_mask = extract_bbox(wnd_mask)
+    wnd_scale, coverage_score, wnd_on_tat_bbox = search_mask_coord(draft, crop_wnd_mask, lower_bound=0.95, upper_bound=0.98)
 
-    # concat tattoo (base layer) and edge (upper layer)
-    draft = load_image(tattoo_path).resize((1024, 1024))
-    mask = load_image(mask_path).resize((1024, 1024))
+    # overlay wound edge to tattoo
+    edge = extract_edge(crop_wnd_mask)
+    draft_with_edge = overlay_edge(draft, edge, wnd_on_tat_bbox)
 
-    _, crop_mask = extract_bbox(mask)
-    mask_scale, coverage_score, bbox_coord = search_mask_coord(draft, crop_mask)
-    edge = extract_edge(crop_mask)
-    draft_with_edge = overlay_edge(draft, edge, bbox_coord)
+    
+    if use_inpaint:
+        moved_wnd_mask = make_moved_wnd_mask(crop_wnd_mask=crop_wnd_mask, wnd_on_tat_bbox=wnd_on_tat_bbox)
 
-    # Sampling image by img2img pipeline
-    tattoo = refiner_pipe(
-        prompt=config.prompt, 
-        image=draft_with_edge, 
-    ).images[0]
-    tattoo.save(tattoo_path)
+        # INPAINTING
+        inpaint_pipe = AutoPipelineForInpainting.from_pretrained(
+            config.inpaint_checkpoint, 
+            torch_dtype=torch.float16, 
+            variant="fp16"
+            ).to(device)
+        
+        tattoo = inpaint_pipe(
+            prompt=args.prompt,
+            image=draft,
+            mask_image=moved_wnd_mask,
+            guidance_scale=config.inpaint_guidance_scale,
+            num_inference_steps=config.inpaint_num_inference_steps,  
+            strength=config.inpaint_strength,  
+        ).images[0]
+    else :
+        # Refine image using img2img pipeline
+        refiner_pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained(
+            config.refiner_checkpoint, 
+            torch_dtype=torch.float16, 
+            variant="fp16", 
+            use_safetensors=True
+        ).to(device)
+        
+        tattoo = refiner_pipe(
+            prompt=config.prompt, 
+            image=draft_with_edge, 
+        ).images[0]
 
-    # save masked wound image 
-    wound = load_image(input_path).resize((1024, 1024))
-    wound = extract_wound(wound, mask)
+
+    # Save results
+    # 1. input image: this line is for resize the image to (1024 x 1024) -> not necessary
+    input_image = load_image(input_path).resize((1024, 1024))
+    input_image.save(input_path)    
+    # 2. wound
+    wound = extract_wound(input_image, wnd_mask)
     wound.save(mask_path)
-
-    # Save integrated results to local
-    results = [extract_edge(mask).resize((1024, 1024)), draft, draft_with_edge, tattoo]
+    # 3. tattoo
+    tattoo.save(tattoo_path)
+    # 4. synthesis tattoo image
+    tat_on_skin = synthesis_tattoo(input_image=input_image, tattoo=tattoo, wnd_bbox=wnd_bbox, wnd_on_tat_bbox=wnd_on_tat_bbox, scale=wnd_scale)
+    tat_on_skin.save(synthesis_path)
+    # 5. integrated results to local
+    results = [extract_edge(wnd_mask).resize((1024, 1024)), draft, draft_with_edge, tattoo]
     make_image_grid(results, rows=1, cols=len(results)).save(results_path)
-    # Log the images and table to wandb
+
+    # Logging the prompt and images to wandb
     table = wandb.Table(columns=[
         'Prompt', 'mask-edge', 'tattoo-design', 'tattoo-and-mask', 'refined-tattoo-design'
     ])
     table.add_data(
-        config.prompt, wandb.Image(extract_edge(mask)), wandb.Image(draft), wandb.Image(draft_with_edge), wandb.Image(tattoo) 
+        config.prompt, wandb.Image(extract_edge(wnd_mask)), wandb.Image(draft), wandb.Image(draft_with_edge), wandb.Image(tattoo) 
     )
-
-    wandb.log({
-        "Prompt-and-Results": table
-    })
+    wandb.log({"Prompt-and-Results": table})
 
     wandb.finish()
+
--- tattoo_generation/utils/mask_processing.py
+++ tattoo_generation/utils/mask_processing.py
@@ -7,13 +7,12 @@ import cv2
 from tqdm import tqdm
 from typing import Tuple, List
 
-from diffusers.utils import load_image, make_image_grid
+from diffusers.utils import load_image
 
 # SAM: segmentation for body part
 import torch
 from torchvision.transforms.functional import to_pil_image
 from transformers import SamModel, SamProcessor
-
 
 
 def extract_bbox(back_and_mask: Image.Image) -> Tuple[List[int], Image.Image]:
@@ -38,6 +37,19 @@ def extract_bbox(back_and_mask: Image.Image) -> Tuple[List[int], Image.Image]:
     return bbox, mask
 
 
+def extract_tattoo_mask_v1(tattoo: Image.Image, tattoo_threshold: int=60) -> Image.Image:
+    # 1. convert tattoo to gray scale image
+    gray_tattoo = np.array(tattoo)
+    gray_tattoo = cv2.cvtColor(gray_tattoo, cv2.COLOR_RGB2GRAY)
+
+    # 2. thresholding
+    _, tattoo_mask = cv2.threshold(gray_tattoo, tattoo_threshold, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    # cv2.imwrite("debug/tattoo_mask.png", tattoo_mask)
+    
+
+    return Image.fromarray(tattoo_mask)
+
+
 def extract_tattoo_mask(tattoo: Image.Image, tattoo_threshold: int=60) -> Image.Image:
     # 1. convert tattoo to gray scale image
     gray_tattoo = np.array(tattoo)
@@ -46,9 +58,10 @@ def extract_tattoo_mask(tattoo: Image.Image, tattoo_threshold: int=60) -> Image.
     # 2. histogram equlization
     gray_tattoo = cv2.equalizeHist(gray_tattoo)
 
-    # 3. thresholding (threshold = 50)
-    _, gray_tattoo = cv2.threshold(gray_tattoo, tattoo_threshold, 255, cv2.THRESH_BINARY_INV)
-
+    # 3. thresholding
+    _, gray_tattoo = cv2.threshold(gray_tattoo, tattoo_threshold, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    # cv2.imwrite("debug/tattoo_mask.png", gray_tattoo)
+    
     # 4. make tattoo mask
     contours, _ = cv2.findContours(gray_tattoo, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -57,22 +70,60 @@ def extract_tattoo_mask(tattoo: Image.Image, tattoo_threshold: int=60) -> Image.
     contour_img = np.zeros_like(gray_tattoo)
     tattoo_mask = cv2.drawContours(contour_img, [contour], -1, (255), thickness=cv2.FILLED)
 
+    kernel_size = 5  # 커널 크기 (너무 크게 설정하면 외곽선이 많이 줄어듦)
+    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    tattoo_mask = cv2.erode(contour_img, kernel, iterations=1)
+
     return Image.fromarray(tattoo_mask)
 
 
-def search_mask_coord(tattoo: Image.Image, mask: Image.Image) -> Tuple[float, float, List[int]]:
+def extract_tattoo_mask_v3(tattoo: Image) -> Image:
+    # Convert PIL Image to numpy array
+    gray_tattoo = np.array(tattoo.convert('L'))  # Convert to grayscale
+    gray_tattoo = cv2.equalizeHist(gray_tattoo)
+    # Apply Gaussian Blur to reduce noise
+    # blurred_img = cv2.GaussianBlur(gray_tattoo, (5, 5), 0)
+
+    # Find Otsu's threshold
+    otsu_threshold, _ = cv2.threshold(gray_tattoo, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    print(otsu_threshold)
+
+    # Apply Canny edge detection using the Otsu's threshold
+    edges = cv2.Canny(gray_tattoo, otsu_threshold * 0.5, otsu_threshold)
+    kernel = np.ones((5, 5), np.uint8)
+    dilated_edges = cv2.dilate(edges, kernel, iterations=1)
+
+    # Find contours from the edges
+    contours, _ = cv2.findContours(dilated_edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Create an empty mask
+    mask = np.zeros_like(gray_tattoo)
+
+    # Draw the contours on the mask
+    cv2.drawContours(mask, contours, -1, (255), thickness=cv2.FILLED)
+
+    # Convert the mask back to PIL Image
+    mask_image = Image.fromarray(mask)
+
+    return mask_image
+
+
+def search_mask_coord(
+        tattoo: Image.Image, 
+        mask: Image.Image, 
+        lower_bound: float=0.95, 
+        upper_bound: float=0.98) -> Tuple[float, float, List[int]]:
     # 1. make tattoo mask
     tattoo_mask = extract_tattoo_mask(tattoo=tattoo).convert('1')
     tattoo_mask = np.array(tattoo_mask)
 
     # 2. mask thresholding (threshold = 1, over => 1, o.w => 0)
-    mask_threshold = 0
+    mask_threshold = 1
     crop_mask = np.array(mask)
     _, crop_mask = cv2.threshold(crop_mask, mask_threshold, 1, cv2.THRESH_BINARY_INV)
     num_white_pixel = np.sum(crop_mask)
 
     # 3. search 
-    lower_bound, upper_bound = 0.85, 0.9
     scale = 1.0 
     best = [1.0, 0.0, []]   # scale, coverage percentage, coordinate 
     mask_shape = crop_mask.shape
@@ -86,7 +137,7 @@ def search_mask_coord(tattoo: Image.Image, mask: Image.Image) -> Tuple[float, fl
 
     # Goal: lower bound <= coverage scorer <= upper bound
     if best[1] < lower_bound: 
-        while 0.7 < scale and best[1] <= lower_bound:
+        while 0.5 < scale and best[1] <= lower_bound:
             scale -= 0.1
             small_mask_shape = (int(mask_shape[0] * scale), int(mask_shape[1] * scale))
             crop_mask = Image.fromarray(crop_mask, mode='L').resize((small_mask_shape[1], small_mask_shape[0]))
@@ -100,7 +151,7 @@ def search_mask_coord(tattoo: Image.Image, mask: Image.Image) -> Tuple[float, fl
 
             print(f'best score: {best[1]}')        
     else:
-        while scale < 1.2 and upper_bound <= best[1]:
+        while scale < 3.0 and upper_bound <= best[1]:
             best = (best[0], lower_bound, best[2])
             scale += 0.1
             big_mask_shape = (int(mask_shape[0] * scale), int(mask_shape[1] * scale))
@@ -136,13 +187,22 @@ def extract_edge(crop_mask: Image.Image) -> Image.Image:
     return edge
 
 
+def make_moved_wnd_mask(crop_wnd_mask: Image.Image, wnd_on_tat_bbox: List[int]) -> Image.Image:
+    moved_wnd_mask = Image.new("L", (1024, 1024))
+    moved_wnd_mask.paste(
+        crop_wnd_mask.resize( (wnd_on_tat_bbox[2] - wnd_on_tat_bbox[0], wnd_on_tat_bbox[3] - wnd_on_tat_bbox[1]) ), 
+        (wnd_on_tat_bbox[0], wnd_on_tat_bbox[1])
+        )
+
+    return moved_wnd_mask
+
+
 def overlay_edge(tattoo: Image.Image, edge: Image.Image, coord: List[int]) -> Image.Image:
-    tat_mask = extract_tattoo_mask(tattoo=tattoo).convert('1')
+    tat_mask = extract_tattoo_mask(tattoo=tattoo)
     
     moved_edge = Image.new("L", (1024, 1024), color="white")
-    moved_edge.paste(edge.resize((coord[3] - coord[1], coord[2] - coord[0])), (coord[1], coord[0]))
-    moved_edge = moved_edge.convert('1')
-    moved_edge = ImageChops.logical_or(tat_mask, moved_edge)
+    moved_edge.paste(edge.resize((coord[2] - coord[0], coord[3] - coord[1])), (coord[0], coord[1]))
+    moved_edge = ImageChops.logical_or(tat_mask.convert('1'), moved_edge.convert('1'))
 
     moved_edge = np.array(moved_edge)
     overlay = np.array(tattoo)
@@ -151,11 +211,12 @@ def overlay_edge(tattoo: Image.Image, edge: Image.Image, coord: List[int]) -> Im
     return Image.fromarray(overlay)
 
 
-def extract_wound(input:Image.Image, mask: Image.Image) -> Image.Image:
+def extract_wound(input: Image.Image, mask: Image.Image) -> Image.Image:
     mask = mask.convert('L')
-    result = Image.composite(input, Image.new('RGB', input.size), mask)
+    wound = deepcopy(input)
+    wound.putalpha(mask)
     
-    return result
+    return wound
 
 
 def extract_skin_mask(image: Image.Image, bbox: List[int]) -> Image.Image:
@@ -196,30 +257,32 @@ def extract_skin_mask(image: Image.Image, bbox: List[int]) -> Image.Image:
     return mask
 
 def synthesis_tattoo(
+        input_image: Image.Image,
         tattoo: Image.Image, 
-        tat_mask: Image.Image, 
-        skin_mask: Image.Image, 
-        wnd_tat_bbox: List[int], 
         wnd_bbox: List[int], 
+        wnd_on_tat_bbox: List[int], 
         scale: int) -> Image.Image:
     width = int(tattoo.size[0] * (1.0 / scale))
     height = int(tattoo.size[1] * (1.0 / scale))
-    resize_tattoo = tattoo.resize((width, height))
-    wnd_tat_bbox = (np.array(wnd_tat_bbox) * (1.0 / scale)).astype(np.intc)
+    resized_tattoo = tattoo.resize((width, height))
+    wnd_on_tat_bbox = (np.array(wnd_on_tat_bbox) * (1.0 / scale)).astype(np.intc)
+
+    skin_mask = extract_skin_mask(image=input_image, bbox=wnd_bbox)
+    tat_mask = extract_tattoo_mask(tattoo=resized_tattoo)
 
     skin_image_frame = (0, 0, skin_mask.size[0], skin_mask.size[1])
 
-    dx = wnd_bbox[0] - wnd_tat_bbox[0]
-    dy = wnd_bbox[1] - wnd_tat_bbox[1]
+    dx = wnd_bbox[0] - wnd_on_tat_bbox[0]
+    dy = wnd_bbox[1] - wnd_on_tat_bbox[1]
     skin_mask_frame = (max(0, dx), max(0, dy), \
-                       min(skin_image_frame[2], resize_tattoo.size[0] + dx), \
-                        min(skin_image_frame[3], resize_tattoo.size[1] + dy))
+                       min(skin_image_frame[2], resized_tattoo.size[0] + dx), \
+                        min(skin_image_frame[3], resized_tattoo.size[1] + dy))
 
-    dx = wnd_tat_bbox[0] - wnd_bbox[0]
-    dy = wnd_tat_bbox[1] - wnd_bbox[1]
+    dx = wnd_on_tat_bbox[0] - wnd_bbox[0]
+    dy = wnd_on_tat_bbox[1] - wnd_bbox[1]
     tat_mask_frame = (max(0, dx), max(0, dy), \
-                      min(resize_tattoo.size[0], skin_image_frame[2] + dx), \
-                      min(resize_tattoo.size[1], skin_image_frame[3] + dy))
+                      min(resized_tattoo.size[0], skin_image_frame[2] + dx), \
+                      min(resized_tattoo.size[1], skin_image_frame[3] + dy))
 
     intersection_mask = ImageChops.logical_and(
         skin_mask.crop(skin_mask_frame).convert('1'),
@@ -229,14 +292,22 @@ def synthesis_tattoo(
     intersection_tat_mask = Image.new('RGB', tat_mask.size)
     intersection_tat_mask.paste(intersection_mask, tat_mask_frame)
 
-    intersection_tat = Image.new('RGB', tattoo.size)
-    intersection_tat.paste(tattoo, (0, 0), intersection_tat_mask.convert("L"))
+    intersection_tat = Image.new('RGB', tat_mask.size)
+    intersection_tat.paste(resized_tattoo, (0, 0), intersection_tat_mask.convert("L"))
     intersection_tat = intersection_tat.crop(tat_mask_frame)
 
     tat_on_skin = Image.new('RGB', skin_mask.size)
     tat_on_skin.paste(intersection_tat, skin_mask_frame)
 
+    intersection_skin_mask = Image.new('RGB', skin_mask.size)
+    intersection_skin_mask.paste(intersection_mask, skin_mask_frame)
+    
+    tat_on_skin.putalpha(intersection_skin_mask.convert("L"))
+
     return tat_on_skin
+
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -247,31 +318,18 @@ if __name__ == "__main__":
 
     user = args.user
     filename = args.file
-    input_path = f'/home/cvmlserver11/junhee/scart/images/{user}/inputs/{filename}'
-    mask_path = f'/home/cvmlserver11/junhee/scart/images/{user}/masks/{filename}'
-    tattoo_path = f'/home/cvmlserver11/junhee/scart/images/{user}/tattoos/{filename}'
+    base_dir = 'path/to/base/dir'
+    input_path = f'{base_dir}/scart/images/{user}/inputs/{filename}'
+    mask_path = f'{base_dir}/scart/images/{user}/masks/{filename}'
+    tattoo_path = f'{base_dir}/scart/images/{user}/tattoos/{filename}'
 
-    image = load_image(input_path).resize((1024, 1024))
+    input_image = load_image(input_path).resize((1024, 1024))
     wnd_mask = load_image(mask_path).resize((1024, 1024))   # type: PIL.Image
     tattoo = load_image(tattoo_path).resize((1024, 1024))   # type: PIL.Image
 
-    wnd_bbox, crop_mask = extract_bbox(wnd_mask)
-    print(f'bbox: {wnd_bbox}')
 
-    scale, score, wnd_on_tat_bbox = search_mask_coord(tattoo, crop_mask)
-    print(f'Moved mask information')
-    print(f'Scale: {scale}')
-    print(f'Coverage score: {score}')
-    print(f'Coordinate: {wnd_on_tat_bbox}\n')
+    extract_tattoo_mask(tattoo=tattoo).save("tat_mask.png")
 
-    print(f"rescaled coord: {(np.array(wnd_on_tat_bbox) * (1.0 / scale)).astype(np.intc)}")
-    print(f'wnd_bbox: {wnd_bbox}\n')
-
-    tat_mask = extract_tattoo_mask(tattoo)
-    tat_mask.save("tattoo_mask.jpg")
-    skin_mask = extract_skin_mask(image, wnd_bbox)
-    skin_mask.save("skin_mask.jpg")
-    tat_on_skin = synthesis_tattoo(tattoo=tattoo, tat_mask=tat_mask, skin_mask=skin_mask, wnd_bbox=wnd_bbox, wnd_tat_bbox=wnd_on_tat_bbox, scale=scale)
-    tat_on_skin.save('tat_on_skin.jpg')
-
+    mask = extract_tattoo_mask(tattoo)
+    mask.save("tat_mask_with_canny.png")
 
